a6b37a1c37813ed64a585a564f02ccb4240cdbb1
Atomic <julien.schmidt@wanadoo.fr>
2010-08-22 20:21:22 +0200

    + Annonces automatiques. + Fichiers SQL du name annonces.

---
+++ sql/GMISLAND/1_name_announce.sql
@@ -0,0 +1,3 @@
+INSERT INTO `command` VALUES ('nameannounce',6,'Syntax: .nameannounce $MessageToBroadcast\r\n\r\nSend a global message to all players online in chat log with colored sender\'s name.');
+INSERT INTO `mangos_string` VALUES ('11000', '|cffffffff[ Maître de jeu : %s ] :|CFF359eff %s|r', null, null, null, null, null, null, null, null);
+INSERT INTO `mangos_string` VALUES ('11001', '|cffffffff[ Administrateur : %s ] :|CFF359eff %s|r', null, null, null, null, null, null, null, null);
---
+++ sql/GMISLAND/2_world_autobroadcast.sql
@@ -0,0 +1,2 @@
+DELETE FROM `mangos_string` WHERE `entry` = 11002;
+INSERT INTO `mangos_string` VALUES (11002, '|cffffcc00[Informations automatique]: |cff00ff00%s|r', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
