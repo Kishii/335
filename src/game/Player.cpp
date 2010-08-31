6803e1b01a51d731e859d56364832e00567d7f78
Atomic <julien.schmidt@wanadoo.fr>
2010-08-31 20:22:22 +0200

    + Victoire en duel ( 52994 ).

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -7016,13 +7016,21 @@ void Player::DuelComplete(DuelCompleteType type)
     {
         GetAchievementMgr().UpdateAchievementCriteria(ACHIEVEMENT_CRITERIA_TYPE_LOSE_DUEL, 1);
         if (duel->opponent)
-            duel->opponent->GetAchievementMgr().UpdateAchievementCriteria(ACHIEVEMENT_CRITERIA_TYPE_WIN_DUEL, 1);
+            {
+                duel->opponent->GetAchievementMgr().UpdateAchievementCriteria(ACHIEVEMENT_CRITERIA_TYPE_WIN_DUEL, 1);
+                //Credit for quest Death's Challenge
+                if (getClass() == CLASS_DEATH_KNIGHT && duel->opponent->GetQuestStatus(12733) == QUEST_STATUS_INCOMPLETE)
+                    duel->opponent->CastSpell(duel->opponent, 52994, true);
+                //Emote for winner. Just fo fun =)
+                duel->opponent->HandleEmoteCommand(EMOTE_ONESHOT_CHEER);
+            }
     }
 
     //Remove Duel Flag object
     GameObject* obj = GetMap()->GetGameObject(GetUInt64Value(PLAYER_DUEL_ARBITER));
     if(obj)
         duel->initiator->RemoveGameObject(obj,true);
+		duel->opponent->RemoveAllSpellCooldown();
 
     /* remove auras */
     std::vector<uint32> auras2remove;
