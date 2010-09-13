718c3928d0e57ee16a80ae7840fb05a179604215
Atomic <julien.schmidt@wanadoo.fr>
2010-09-13 21:04:45 +0200

    + Foi distordue ( 51167 ).

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -7306,7 +7306,10 @@ uint32 Unit::SpellHealingBonusDone(Unit *pVictim, SpellEntry const *spellProto, 
     AuraList const& mOverrideClassScript= owner->GetAurasByType(SPELL_AURA_OVERRIDE_CLASS_SCRIPTS);
     for(AuraList::const_iterator i = mOverrideClassScript.begin(); i != mOverrideClassScript.end(); ++i)
     {
-        if (!(*i)->isAffectedOnSpell(spellProto))
+        if (!(*i)->isAffectedOnSpell(spellProto)       
+            // this is very ugly hack for Twisted Faith not affecting Mind Flay
+            && !( (*i)->GetModifier()->m_miscvalue == 7377 && spellProto->SpellFamilyName == SPELLFAMILY_PRIEST &&
+                (spellProto->SpellFamilyFlags & UI64LIT(0x800000))) )
             continue;
         switch((*i)->GetModifier()->m_miscvalue)
         {
