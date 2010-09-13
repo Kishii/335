2d9fa93102b4e6425f98fac4fc2459cc458955c1
Atomic <julien.schmidt@wanadoo.fr>
2010-09-13 21:30:42 +0200

    +/- Revitaliser ( 48542 ).

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -4027,12 +4027,14 @@ void Spell::EffectEnergize(SpellEffectIndex eff_idx)
             level_multiplier = 4;
             break;
         case 31930:                                         // Judgements of the Wise
-        case 48542:                                         // Revitalize (mana restore case)
         case 63375:                                         // Improved Stormstrike
         case 67545:                                         // Empowered Fire
         case 68082:                                         // Glyph of Seal of Command
         case 71132:                                         // Glyph of Shadow Word: Pain
             damage = damage * unitTarget->GetCreateMana() / 100;
+            break;
+        case 48542:                                         // Revitalize (mana restore case)
+            damage = damage * unitTarget->GetMaxPower(POWER_MANA) / 100;
             break;
         case 67487:                                         // Mana Potion Injector
         case 67490:                                         // Runic Mana Injector
